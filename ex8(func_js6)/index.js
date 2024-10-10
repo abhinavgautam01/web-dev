cddc113dca081baf02a105fcf06ac32be207afac
abhinavgautam01 <abhinavgautam1017@gmail.com>
2024-10-10 16:51:38 +0530

    js_practice

--- ex8(func_js6)/index.js
+++ ex8(func_js6)/index.js
@@ -3,18 +3,24 @@ function nice(name) {
     console.log("Hey "+name+" you are good")
     console.log("Hey "+name+" your tshirt is nice")
 }
+nice("Abhinav")
+console.log("..........")
+nice("Golu")
+
+
 function sum(a,b,c=3) {     //c is default parameter
     // console.log(a+b)
     console.log(a,b,c)
     return a+b+c
 }
-nice("Abhinav")
-console.log("..........")
-nice("Golu")
 result1=sum(3,5,2)
 result2=sum("abc","def")
 result3=sum("abc",5)
 result4=sum(5)
+console.log("..........")
+
+
+
 console.log("The sum of the number is : ",result1)
 console.log("The sum of the string is : ",result2)
 console.log("The sum of the string and number is : ",result3)
