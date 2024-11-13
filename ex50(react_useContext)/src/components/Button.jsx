6ad2d746d6288089327be9d1c843465d4b0e9a86
abhinavgautam01 <abgautam1017@gmail.com>
2024-11-13 23:13:15 +0530

    not-completed-yet

---
+++ ex50(react_useContext)/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Component1 from './component1'
+
+const Button = ({count}) => {
+  return (
+    
+    <div>
+      <button>
+        <span>
+            <Component1 count={count}/>
+            </span>
+            I am a button
+            </button>
+    </div>
+    
+  )
+}
+
+export default Button
---
+++ ex50(react_useContext)/src/components/Component1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Component1 = ({count}) => {
+  return (
+    <div>
+        {count}
+        console.log()
+    </div>
+  )
+}
+
+export default Component1
