f40c790cbf736961076db65d1d2bebfa0a6c2ef5
abhinavgautam01 <abgautam1017@gmail.com>
2024-11-15 22:30:33 +0530

    nextJs-backend-api

---
+++ ex58-nextJs-backend-api/my-app/app/page.js
@@ -0,0 +1,30 @@
+"use client"
+import Image from "next/image";
+
+export default function Home() {
+  
+  const handleClick = async() => {
+    
+    let data = {
+      name: "Golu",
+      role: "Coder"
+    }
+
+    let a = await fetch("/api/add", {
+      method: "POST", headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    let res = await a.json()
+    console.log(res)
+  }
+  return (
+   <div>
+    <h1 className="text-x1 font-bold">
+      Next.js Api demo
+    </h1>
+    <button onClick={handleClick}>Click me</button>
+   </div>
+  );
+}
