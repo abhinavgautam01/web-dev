f0aed0cf6963b6ef59c976e314c5baf798266405
abhinavgautam01 <abgautam1017@gmail.com>
2024-12-15 17:42:26 +0530

    typescript

---
+++ typescript-ex2/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const students = [
+    {
+        name: "John",
+        score: 70,
+    },
+    {
+        name: "Alice",
+        score: 80,
+    },
+    {
+        name: "Golu",
+        score: 75,
+    },
+];
+for (const student of students) {
+    console.log("Name : ", student.name);
+    console.log("Score : ", student.score);
+}
