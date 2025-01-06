e910c6e6840bbb0743305c40ce837a29dd83a1a2
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-06 21:18:30 +0530

    typescript

--- typescript-ex18/index.js
+++ typescript-ex18/index.js
@@ -1,15 +1,28 @@
 "use strict";
-const map = (array, func) => {
-    if (array.length === 0) {
-        return array;
+// const map = <T, U>(array: T[], func:(item:T)=>U): (U|T)[]=>{
+//     if(array.length === 0){
+//         return array
+//     }
+//     let result: U[] = []
+//     for(let i = 0; i<array.length; i++){
+//         result.push(func(array[i]))
+//     }
+//     return result
+// }
+// let numbers = [4, 5, 6, 7, 8, 9]
+// const converted = map(numbers, (num)=>num.toString())
+// console.log("numnbers[]: ", numbers);
+// console.log("converted: ", converted);
+//classes
+class User {
+    constructor() {
+        this.name = "Golu";
+        this.email = "golu@gmail.com";
     }
-    let result = [];
-    for (let i = 0; i < array.length; i++) {
-        result.push(func(array[i]));
+    greet() {
+        return 'Hello Golu';
     }
-    return result;
-};
-let numbers = [4, 5, 6, 7, 8, 9];
-const converted = map(numbers, (num) => num.toString());
-console.log("numnbers[]: ", numbers);
-console.log("converted: ", converted);
+}
+const user = new User();
+console.log("User: ", user);
+console.log("User: ", user.greet());
--- typescript-ex18/index.ts
+++ typescript-ex18/index.ts
@@ -1,17 +1,29 @@
 
-const map = <T, U>(array: T[], func:(item:T)=>U): (U|T)[]=>{
-    if(array.length === 0){
-        return array
+// const map = <T, U>(array: T[], func:(item:T)=>U): (U|T)[]=>{
+//     if(array.length === 0){
+//         return array
+//     }
+//     let result: U[] = []
+//     for(let i = 0; i<array.length; i++){
+//         result.push(func(array[i]))
+//     }
+//     return result
+// }
+
+// let numbers = [4, 5, 6, 7, 8, 9]
+// const converted = map(numbers, (num)=>num.toString())
+// console.log("numnbers[]: ", numbers);
+// console.log("converted: ", converted);
+
+//classes
+
+class User{
+    name = "Golu"
+    email = "golu@gmail.com"
+    greet(){
+        return 'Hello Golu'
     }
-    let result: U[] = []
-    for(let i = 0; i<array.length; i++){
-        result.push(func(array[i]))
-    }
-    return result
 }
-
-let numbers = [4, 5, 6, 7, 8, 9]
-const converted = map(numbers, (num)=>num.toString())
-console.log("numnbers[]: ", numbers);
-console.log("converted: ", converted);
-
+const user = new User()
+console.log("User: ",user);
+console.log("User: ",user.greet());
