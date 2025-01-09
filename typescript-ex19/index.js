ead7d24a41f2694d1f53f265d4f3bc3d788ba2ea
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-09 11:59:16 +0530

    typescript

--- typescript-ex19/index.js
+++ typescript-ex19/index.js
@@ -1,8 +1,9 @@
 "use strict";
 class User {
-    constructor(name, email) {
+    constructor(name, email, lastname) {
         this.name = name;
         this.email = email;
+        this.lastname = lastname;
     }
     greet() {
         return `Hello ${this.name}`;
@@ -10,7 +11,20 @@ class User {
 }
 const userOne = new User("Golu", "golu@gmail.com");
 const userTwo = new User("Gautam", "gautam@gmail.com");
+const userThree = new User("Abhinav", "abhinav@gmail.com", "Gautam");
+userOne.lastname = "abc";
 console.log("userOne: ", userOne);
 console.log("userOne.greet(): ", userOne.greet());
 console.log("userTwo: ", userTwo);
 console.log("userTwo.greet(): ", userTwo.greet());
+console.log("userThree: ", userThree);
+console.log("userThree.greet(): ", userThree.greet());
+//inheritance
+class Admin extends User {
+    constructor() {
+        super(...arguments);
+        this.isAdmin = true;
+    }
+}
+const admin = new Admin("admin", "admin@gmail.com", "hai");
+console.log("admin: ", admin);
--- typescript-ex19/index.ts
+++ typescript-ex19/index.ts
@@ -1,9 +1,11 @@
 class User {
     name: string
-    email: string
-    constructor(name: string, email: string) {
+    readonly email: string
+    lastname?: string
+    constructor(name: string, email: string, lastname?: string) {
         this.name = name
         this.email = email
+        this.lastname = lastname
     }
     greet(){
         return `Hello ${this.name}`
@@ -11,8 +13,19 @@ class User {
 }
 const userOne = new User("Golu", "golu@gmail.com")
 const userTwo = new User("Gautam", "gautam@gmail.com")
+const userThree = new User("Abhinav", "abhinav@gmail.com", "Gautam")
+userOne.lastname = "abc"
+
 console.log("userOne: ",userOne);
 console.log("userOne.greet(): ",userOne.greet());
 console.log("userTwo: ",userTwo);
 console.log("userTwo.greet(): ",userTwo.greet());
+console.log("userThree: ",userThree);
+console.log("userThree.greet(): ",userThree.greet());
 
+//inheritance
+class Admin extends User {
+    isAdmin: boolean = true
+}
+const admin: Admin = new Admin("admin", "admin@gmail.com", "hai")
+console.log("admin: ", admin);
