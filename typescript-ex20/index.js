f4dbc8a906558862d9e25194e0b7b479b496d21e
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-13 15:22:20 +0530

    typescript

--- typescript-ex20/index.js
+++ typescript-ex20/index.js
@@ -2,9 +2,10 @@
 //access modifiers...!
 //by default public
 class User {
-    constructor(name, email, lastName) {
+    constructor(name, email, phone, lastName) {
         this.name = name;
         this.email = email;
+        this.phone = phone;
         if (lastName) {
             this.lastName = lastName;
         }
@@ -13,21 +14,31 @@ class User {
         return `Hello ${this.name}`;
     }
 }
-const user = new User("Golu", "golu@gmail.com");
+const user = new User("Golu", "golu@gmail.com", 1234);
 console.log("User: ", user);
 class Admin extends User {
-    constructor(name, email, userReporting, lastName) {
-        super(name, email, lastName);
+    constructor(name, email, phone, userReporting, lastName) {
+        super(name, email, phone, lastName);
         this.isAdmin = true;
         this.userReporting = userReporting;
     }
     printName() {
         console.log(this.name);
     }
+    printPhone() {
+        console.log(this.phone);
+    }
+    useProtectedPhone() {
+        this.printPhone();
+    }
 }
-const adminUser = new Admin("Abhinav", "abhinav@gmail.com", 3, "Gautam");
+const adminUser = new Admin("Abhinav", "abhinav@gmail.com", 8291, 3, "Gautam");
 console.log("Admin: ", adminUser);
 console.log("----");
 adminUser.printName();
 console.log("----");
 console.log(user.name);
+//protected members..!
+console.log("----");
+console.log("phone: ");
+adminUser.useProtectedPhone();
--- typescript-ex20/index.ts
+++ typescript-ex20/index.ts
@@ -5,10 +5,12 @@ class User{
     public name: string
     readonly email: string
     lastName?: string
+    protected phone: number
 
-    constructor(name: string, email: string, lastName?: string){
+    constructor(name: string, email: string, phone:number, lastName?: string){
         this.name = name
         this.email = email
+        this.phone = phone
         if(lastName){
             this.lastName = lastName
         }
@@ -17,24 +19,35 @@ class User{
         return `Hello ${this.name}`
     }
 }
-const user: User = new User("Golu", "golu@gmail.com")
+const user: User = new User("Golu", "golu@gmail.com", 1234)
 console.log("User: ", user);
 
 class Admin extends User{
     isAdmin: boolean = true
     userReporting: number
-    constructor(name: string, email: string, userReporting: number, lastName?: string){
-        super(name, email, lastName)
+    constructor(name: string, email: string, phone:number, userReporting: number, lastName?: string){
+        super(name, email, phone, lastName)
         this.userReporting = userReporting
     }
     public printName(){
         console.log(this.name);
     }
+    protected printPhone(){
+        console.log(this.phone);
+    }
+    public useProtectedPhone(){
+        this.printPhone()
+    }
 }
 
-const adminUser: Admin = new Admin("Abhinav", "abhinav@gmail.com", 3, "Gautam")
+const adminUser: Admin = new Admin("Abhinav", "abhinav@gmail.com", 8291, 3, "Gautam")
 console.log("Admin: ", adminUser);
 console.log("----");
 adminUser.printName()
 console.log("----");
 console.log(user.name);
+
+//protected members..!
+console.log("----");
+console.log("phone: ");
+adminUser.useProtectedPhone()
