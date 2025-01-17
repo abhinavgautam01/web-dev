d2244da46dd042924cf5fb2807630e81ffb0298f
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-17 21:23:18 +0530

    typescript

--- typescript-ex21/index.js
+++ typescript-ex21/index.js
@@ -1,16 +1,21 @@
 "use strict";
 class Person {
-    constructor(firstName, lastName, age) {
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.age = age;
+    }
+    //setter or mutator
+    set age(age) {
         if (age > 200 || age < 1) {
             throw new Error("The age must be within age range 0-200");
         }
+        this._age = age;
     }
     fullName() {
         return `${this.firstName} ${this.lastName}`;
     }
 }
-const person = new Person("Abhinav", "Gautam", 20);
+const person = new Person("Abhinav", "Gautam");
 console.log("person.fullName() :", person.fullName());
+person.age = 40;
+console.log(person.age);
--- typescript-ex21/index.ts
+++ typescript-ex21/index.ts
@@ -1,16 +1,24 @@
 class Person{
+    private _age?: number
     constructor(
         public firstName: string,
         public lastName: string,
-        public age: number
     ){
+    }
+    
+    //setter or mutator
+    public set age(age: number){
         if(age>200 || age <1){
             throw new Error("The age must be within age range 0-200")
         }
+        this._age = age
     }
+
     public fullName(){
     return `${this.firstName} ${this.lastName}`
     }
 }
-const person: Person = new Person("Abhinav", "Gautam", 20)
+const person: Person = new Person("Abhinav", "Gautam")
 console.log("person.fullName() :",person.fullName());
+person.age = 40
+console.log(person.age);
