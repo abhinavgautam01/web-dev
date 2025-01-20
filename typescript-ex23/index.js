8e6ea0a75e6b84bb407dbd74cc127097656a1df7
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-20 13:44:28 +0530

    typescript

--- typescript-ex23/index.js
+++ typescript-ex23/index.js
@@ -29,3 +29,47 @@ const user = new UserWithTimeStamp("Golu", 20);
 console.log("user: ", user);
 console.log("----");
 user.displayInfo();
+class Employee {
+    constructor(id, name, _salary, age) {
+        this.id = id;
+        this.name = name;
+        this._salary = _salary;
+        this.age = age;
+    }
+    set salary(salary) {
+        if (salary > 0) {
+            this._salary = salary;
+        }
+        else {
+            throw new Error("Salary must be a positive number..!");
+        }
+    }
+    get salary() {
+        return this._salary;
+    }
+    static getCompanyName() {
+        return Employee.companyName;
+    }
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}, Salary: ${this.salary}`;
+    }
+}
+Employee.companyName = "Tech Solutions Inc.";
+class Manager extends Employee {
+    constructor(id, name, _salary, age, department) {
+        super(id, name, _salary, age);
+        this.department = department;
+        this.department = department;
+    }
+    getDetails() {
+        return `${super.getDetails()}, Department: ${this.department}`;
+    }
+}
+const emp = new Employee(1, "abc", 1234, 20);
+console.log("---");
+console.log("emp.getDetails(): ", emp.getDetails());
+emp.salary = 12;
+console.log("emp.getDetails(): ", emp.getDetails());
+const manager1 = new Manager(1, "abc", 1234, 20, "computer Science");
+console.log("---");
+console.log("manager1.getDetails(): ", manager1.getDetails());
--- typescript-ex23/index.ts
+++ typescript-ex23/index.ts
@@ -27,3 +27,50 @@ const user: UserWithTimeStamp = new UserWithTimeStamp("Golu", 20)
 console.log("user: ", user);
 console.log("----");
 user.displayInfo();
+
+class Employee{
+    public static companyName: string = "Tech Solutions Inc."
+    constructor(protected id: number, public name: string, private _salary: number, public age: number){}
+    public set salary(salary: number){
+        if(salary>0){
+            this._salary = salary
+        }
+        else{
+            throw new Error("Salary must be a positive number..!")
+        }
+    }
+    public get salary(){
+        return this._salary
+    }
+    public static getCompanyName(){
+        return Employee.companyName
+    }
+    getDetails(): string{
+        return `Name: ${this.name}, Age: ${this.age}, Salary: ${this.salary}`
+    }
+}
+class Manager extends Employee{
+    constructor(
+        id: number,
+        name: string,
+        _salary: number,
+        age: number,
+        public department: string
+    )
+    {
+        super(id, name, _salary, age)
+        this.department = department
+    }
+    getDetails(): string{
+        return `${super.getDetails()}, Department: ${this.department}`
+    }
+}
+const emp = new Employee(1, "abc", 1234, 20)
+console.log("---");
+console.log("emp.getDetails(): ",emp.getDetails());
+emp.salary = 12
+console.log("emp.getDetails(): ",emp.getDetails());
+
+const manager1 = new Manager(1, "abc", 1234, 20, "computer Science")
+console.log("---");
+console.log("manager1.getDetails(): ",manager1.getDetails());
