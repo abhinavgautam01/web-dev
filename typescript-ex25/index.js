4b204dc1040e849702e6de0b632d3fa17c7e28f3
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-22 12:06:12 +0530

    typescript

--- typescript-ex25/index.js
+++ typescript-ex25/index.js
@@ -30,8 +30,86 @@ Auth.login(customer);
 const personWithAddress = {
     address: "Kullu",
     name: "Golu",
-    age: 20,
+    gender: "Male",
     email: "golu@gmail.com",
     phone: 1234
 };
 console.log("personWithAddress: ", personWithAddress);
+var Roles;
+(function (Roles) {
+    Roles["admin"] = "admin";
+    Roles["author"] = "author";
+    Roles["editor"] = "editor";
+})(Roles || (Roles = {}));
+var PermissionList;
+(function (PermissionList) {
+    PermissionList["read"] = "read";
+    PermissionList["write"] = "write";
+    PermissionList["execute"] = "execute";
+})(PermissionList || (PermissionList = {}));
+const adminUser = {
+    name: "Abhinav Gautam",
+    gender: "Male",
+    email: "abhinavgautam@gmail.com",
+    phone: 9876,
+    role: Roles.admin,
+    permissions: [PermissionList.read, PermissionList.write, PermissionList.execute],
+    numberOfUsersReporting: 2
+};
+console.log("adminUser: ", adminUser);
+var AutomobileTypes;
+(function (AutomobileTypes) {
+    AutomobileTypes["car"] = "car";
+    AutomobileTypes["bus"] = "bus";
+    AutomobileTypes["van"] = "van";
+    AutomobileTypes["truck"] = "truck";
+    AutomobileTypes["bike"] = "bike";
+})(AutomobileTypes || (AutomobileTypes = {}));
+var AutomobileBrand;
+(function (AutomobileBrand) {
+    AutomobileBrand["ferrari"] = "ferrari";
+    AutomobileBrand["honda"] = "honda";
+    AutomobileBrand["bmw"] = "bmw";
+    AutomobileBrand["toyota"] = "toyota";
+})(AutomobileBrand || (AutomobileBrand = {}));
+var AutomobileColors;
+(function (AutomobileColors) {
+    AutomobileColors["red"] = "red";
+    AutomobileColors["blue"] = "blue";
+    AutomobileColors["white"] = "white";
+    AutomobileColors["black"] = "black";
+    AutomobileColors["silver"] = "silver";
+})(AutomobileColors || (AutomobileColors = {}));
+const ferrari = {
+    type: AutomobileTypes.car,
+    brand: AutomobileBrand.ferrari,
+    colors: [AutomobileColors.black, AutomobileColors.red],
+    description: "This is a ferrari"
+};
+const honda = {
+    type: "car",
+    brand: "Honda",
+    colors: ["silver", "black"],
+    description: "this is a Honda"
+};
+const toyota = {
+    type: "car",
+    brand: AutomobileBrand.honda,
+    colors: [6678, 2234],
+    description: "this is a Toyota"
+};
+console.log("---");
+console.log("ferrari: ", ferrari);
+console.log("honda: ", honda);
+console.log("toyota: ", toyota);
+console.log("---");
+class Car {
+    constructor(brand, colors, description) {
+        this.brand = brand;
+        this.colors = colors;
+        this.description = description;
+        this.type = "car";
+    }
+}
+const newFerrari = new Car(AutomobileBrand.ferrari, [AutomobileColors.red, AutomobileColors.black], "This is new ferrari");
+console.log("newFerrari:", newFerrari);
--- typescript-ex25/index.ts
+++ typescript-ex25/index.ts
@@ -25,9 +25,10 @@ const admin: Admin = new Admin("Golu", "golu@gmail.com", 1)
 const customer: Customer = new Customer("John", "john@gmail.com")
 Auth.login(admin)
 Auth.login(customer)
+
 interface Person{
     name: string
-    age: number
+    gender: string
     email: string
     phone: number
 }
@@ -39,8 +40,107 @@ interface PersonWithAddress extends Person{
 const personWithAddress: PersonWithAddress = {
     address: "Kullu",
     name: "Golu",
-    age: 20,
+    gender: "Male",
     email: "golu@gmail.com",
     phone: 1234
 }
 console.log("personWithAddress: ", personWithAddress);
+
+enum Roles {
+    admin = "admin",
+    author = "author",
+    editor = "editor"
+}
+interface Role{
+    role: Roles
+}
+
+enum PermissionList{
+    read = "read",
+    write = "write",
+    execute = "execute"
+}
+interface UserPermissions{
+    permissions: PermissionList[]
+}
+
+interface AdminUser extends Person, Role, UserPermissions{
+    numberOfUsersReporting: number
+}
+
+const adminUser: AdminUser = {
+    name: "Abhinav Gautam",
+    gender: "Male",
+    email: "abhinavgautam@gmail.com",
+    phone: 9876,
+    role: Roles.admin,
+    permissions: [PermissionList.read, PermissionList.write, PermissionList.execute],
+    numberOfUsersReporting: 2
+}
+console.log("adminUser: ", adminUser);
+
+//interface with generics
+interface AutoMobile<Type, Brand, Colors> {
+    type: Type
+    brand: Brand
+    colors: Colors[]
+    description: string
+}
+
+enum AutomobileTypes{
+    car = "car",
+    bus = "bus",
+    van = "van",
+    truck = "truck",
+    bike = "bike"
+}
+
+enum AutomobileBrand {
+    ferrari = "ferrari",
+    honda = "honda",
+    bmw = "bmw",
+    toyota = "toyota"
+}
+
+enum AutomobileColors {
+    red = "red",
+    blue = "blue",
+    white = "white",
+    black = "black",
+    silver = "silver"
+}
+
+const ferrari: AutoMobile<AutomobileTypes, AutomobileBrand, AutomobileColors> = {
+    type: AutomobileTypes.car,
+    brand: AutomobileBrand.ferrari,
+    colors: [AutomobileColors.black, AutomobileColors.red],
+    description: "This is a ferrari"
+}
+const honda: AutoMobile<string, string, string>={
+    type: "car",
+    brand: "Honda",
+    colors: ["silver","black"],
+    description: "this is a Honda"
+}
+const toyota: AutoMobile<string, AutomobileBrand, number>={
+    type: "car",
+    brand: AutomobileBrand.honda,
+    colors: [6678, 2234],
+    description: "this is a Toyota"
+}
+console.log("---");
+console.log("ferrari: ", ferrari);
+console.log("honda: ", honda);
+console.log("toyota: ", toyota);
+console.log("---");
+
+class Car implements AutoMobile<string, AutomobileBrand, AutomobileColors>{
+    public type: string = "car"
+    constructor(public brand: AutomobileBrand, public colors: AutomobileColors[], public description: string){
+
+    }
+}
+
+const newFerrari: Car = new Car(AutomobileBrand.ferrari, [AutomobileColors.red, AutomobileColors.black], "This is new ferrari")
+console.log("newFerrari:", newFerrari);
+
