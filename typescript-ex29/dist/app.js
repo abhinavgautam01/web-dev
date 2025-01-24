7b2515b4481582d88f48168ef406e5f89bd16e87
abhinavgautam01 <abgautam1017@gmail.com>
2025-01-24 09:48:43 +0530

    typescript

--- typescript-ex29/dist/app.js
+++ typescript-ex29/dist/app.js
@@ -1,4 +1,8 @@
 "use strict";
+// enum Manufacturers {
+//     boeing = 'boeing',
+//     airbus = 'airbus'
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,82 +12,117 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var Manufacturers;
-(function (Manufacturers) {
-    Manufacturers["boeing"] = "boeing";
-    Manufacturers["airbus"] = "airbus";
-})(Manufacturers || (Manufacturers = {}));
-function StaticMethodDecorator(constructor, methodName, descriptor) {
-    console.log(constructor);
-    console.log(methodName);
-    console.log(descriptor);
-    descriptor.writable = false;
-}
-function MethodDecorator(classPrototype, methodName, descriptor) {
-    console.log(classPrototype);
-    console.log(methodName);
-    console.log(descriptor);
-    // descriptor.writable = false
-}
-function ParameterDecorator(classPrototype, methodName, index) {
-    console.log(classPrototype);
-    console.log(methodName);
-    console.log(index);
-}
-function PropertyDecorator(classPrototype, propertyName) {
-    console.log(classPrototype);
-    console.log(propertyName);
-}
-function AccessorDecorator(classPrototype, accessorName, propertyDescriptor) {
-    console.log(classPrototype);
-    console.log(accessorName);
-    console.log(propertyDescriptor);
-}
-class Airplane {
-    constructor(_aircraftModel) {
-        this._aircraftModel = this.aircraftModel;
-    }
-    // @StaticMethodDecorator
-    static seatCount() {
-        console.log("150 seats");
-    }
-    // @MethodDecorator
-    pilotName(name) {
-        console.log(name);
-    }
-    get aircraftModel() {
-        return this._aircraftModel;
-    }
-}
-__decorate([
-    PropertyDecorator,
-    __metadata("design:type", String)
-], Airplane.prototype, "_aircraftModel", void 0);
-__decorate([
-    AccessorDecorator,
-    __metadata("design:type", Object),
-    __metadata("design:paramtypes", [])
-], Airplane.prototype, "aircraftModel", null);
-function AddLocation(lat, long) {
-    return (classConstructor) => {
-        return class extends classConstructor {
-            constructor(...args) {
-                super(...args);
-                this.mapLocation = { lat, long };
-            }
-        };
+// interface AircraftInterface {
+//     _aircraftModel: string
+//     pilotName: (name: string, lastName: string)=>void
+//     prototype?: any
+//     origin?: string
+//     manufacturer?: string
+//     type?: string
+//     airbusMethod?: () => void
+//     boiengMethod?: () => void
+// }
+// function StaticMethodDecorator(constructor: Object, methodName: string, descriptor: PropertyDescriptor){
+//     console.log(constructor);
+//     console.log(methodName);
+//     console.log(descriptor);
+//     descriptor.writable = false
+// }
+// function MethodDecorator(classPrototype: Object, methodName: string, descriptor: PropertyDescriptor){
+//     console.log(classPrototype);
+//     console.log(methodName);
+//     console.log(descriptor);
+//     // descriptor.writable = false
+// }
+// function ParameterDecorator(classPrototype: Object, methodName: string, index: number){
+//     console.log(classPrototype);
+//     console.log(methodName);
+//     console.log(index);
+// }
+// function PropertyDecorator(classPrototype: Object, propertyName:string){
+//     console.log(classPrototype);
+//     console.log(propertyName);
+// }
+// function AccessorDecorator(classPrototype: Object, accessorName: string, propertyDescriptor: PropertyDescriptor){
+//     console.log(classPrototype);
+//     console.log(accessorName);
+//     console.log(propertyDescriptor);
+// }
+// class Airplane implements AircraftInterface {
+//     @PropertyDecorator
+//     public _aircraftModel: string
+//     constructor(
+//         _aircraftModel: string,
+//     ) {
+//         this._aircraftModel = this.aircraftModel
+//     }
+//     // @StaticMethodDecorator
+//     public static seatCount(): void{
+//         console.log("150 seats");
+//     }
+//     // @MethodDecorator
+//     public pilotName(name: string, /*@ParameterDecorator lastName: string*/) {
+//         console.log(name);
+//     }
+//     @AccessorDecorator
+//     public get aircraftModel() {
+//         return this._aircraftModel
+//     }
+// }
+// //for static method or property, you wil always get constructor as first argument in decorators
+// interface MapLocation {
+//     lat: number,
+//     long: number
+// }
+// function AddLocation(lat: number, long: number){
+//     return <T extends {new(...args: any[]): {}}>(classConstructor: T)=>{
+//         return class extends classConstructor{
+//             public mapLocation: MapLocation;
+//             constructor(...args: any[]){
+//                 super(...args)
+//                 this.mapLocation = {lat, long}
+//             }
+//         }
+//     }
+// }
+// @AddLocation(1.234, 1.876)
+// class Person{
+//     constructor(
+//         public name: string, 
+//         public age: number, 
+//     ){
+//     }
+// }
+// const person: Person = new Person("John", 20)
+// console.log(person);
+function first() {
+    console.log("first(): factory evaluated");
+    return function (target, propertyKey, descriptor) {
+        console.log("first(): called");
+        console.log(target);
+        console.log(propertyKey);
+        console.log(descriptor);
+        console.log("---");
     };
 }
-let Person = class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-};
-Person = __decorate([
-    AddLocation(1.234, 1.876),
-    __metadata("design:paramtypes", [String, Number])
-], Person);
-const person = new Person("John", 20);
-console.log(person);
+function second() {
+    console.log("second(): factory evaluated");
+    return function (target, propertyKey, descriptor) {
+        console.log("second(): called");
+        console.log(target);
+        console.log(propertyKey);
+        console.log(descriptor);
+        console.log("---");
+    };
+}
+class ExampleClass {
+    method() { }
+}
+__decorate([
+    first(),
+    second(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], ExampleClass.prototype, "method", null);
 //# sourceMappingURL=app.js.map
--- typescript-ex29/src/app.ts
+++ typescript-ex29/src/app.ts
@@ -1,101 +1,130 @@
-enum Manufacturers {
-    boeing = 'boeing',
-    airbus = 'airbus'
-}
+// enum Manufacturers {
+//     boeing = 'boeing',
+//     airbus = 'airbus'
+// }
 
-interface AircraftInterface {
-    _aircraftModel: string
-    pilotName: (name: string, lastName: string)=>void
-    prototype?: any
-    origin?: string
-    manufacturer?: string
-    type?: string
-    airbusMethod?: () => void
-    boiengMethod?: () => void
-}
+// interface AircraftInterface {
+//     _aircraftModel: string
+//     pilotName: (name: string, lastName: string)=>void
+//     prototype?: any
+//     origin?: string
+//     manufacturer?: string
+//     type?: string
+//     airbusMethod?: () => void
+//     boiengMethod?: () => void
+// }
 
-function StaticMethodDecorator(constructor: Object, methodName: string, descriptor: PropertyDescriptor){
-    console.log(constructor);
-    console.log(methodName);
-    console.log(descriptor);
-    descriptor.writable = false
-}
-function MethodDecorator(classPrototype: Object, methodName: string, descriptor: PropertyDescriptor){
-    console.log(classPrototype);
-    console.log(methodName);
-    console.log(descriptor);
-    // descriptor.writable = false
-}
-function ParameterDecorator(classPrototype: Object, methodName: string, index: number){
-    console.log(classPrototype);
-    console.log(methodName);
-    console.log(index);
-}
-function PropertyDecorator(classPrototype: Object, propertyName:string){
-    console.log(classPrototype);
-    console.log(propertyName);
-}
-function AccessorDecorator(classPrototype: Object, accessorName: string, propertyDescriptor: PropertyDescriptor){
-    console.log(classPrototype);
-    console.log(accessorName);
-    console.log(propertyDescriptor);
-}
+// function StaticMethodDecorator(constructor: Object, methodName: string, descriptor: PropertyDescriptor){
+//     console.log(constructor);
+//     console.log(methodName);
+//     console.log(descriptor);
+//     descriptor.writable = false
+// }
+// function MethodDecorator(classPrototype: Object, methodName: string, descriptor: PropertyDescriptor){
+//     console.log(classPrototype);
+//     console.log(methodName);
+//     console.log(descriptor);
+//     // descriptor.writable = false
+// }
+// function ParameterDecorator(classPrototype: Object, methodName: string, index: number){
+//     console.log(classPrototype);
+//     console.log(methodName);
+//     console.log(index);
+// }
+// function PropertyDecorator(classPrototype: Object, propertyName:string){
+//     console.log(classPrototype);
+//     console.log(propertyName);
+// }
+// function AccessorDecorator(classPrototype: Object, accessorName: string, propertyDescriptor: PropertyDescriptor){
+//     console.log(classPrototype);
+//     console.log(accessorName);
+//     console.log(propertyDescriptor);
+// }
 
-class Airplane implements AircraftInterface {
-    @PropertyDecorator
-    public _aircraftModel: string
-    constructor(
-        _aircraftModel: string,
-    ) {
-        this._aircraftModel = this.aircraftModel
+// class Airplane implements AircraftInterface {
+//     @PropertyDecorator
+//     public _aircraftModel: string
+//     constructor(
+//         _aircraftModel: string,
+//     ) {
+//         this._aircraftModel = this.aircraftModel
+//     }
+
+//     // @StaticMethodDecorator
+//     public static seatCount(): void{
+//         console.log("150 seats");
+//     }
+
+//     // @MethodDecorator
+//     public pilotName(name: string, /*@ParameterDecorator lastName: string*/) {
+//         console.log(name);
+//     }
+
+//     @AccessorDecorator
+//     public get aircraftModel() {
+//         return this._aircraftModel
+//     }
+// }
+
+
+
+// //for static method or property, you wil always get constructor as first argument in decorators
+
+
+// interface MapLocation {
+//     lat: number,
+//     long: number
+// }
+
+// function AddLocation(lat: number, long: number){
+//     return <T extends {new(...args: any[]): {}}>(classConstructor: T)=>{
+//         return class extends classConstructor{
+//             public mapLocation: MapLocation;
+//             constructor(...args: any[]){
+//                 super(...args)
+//                 this.mapLocation = {lat, long}
+//             }
+//         }
+//     }
+// }
+
+// @AddLocation(1.234, 1.876)
+// class Person{
+//     constructor(
+//         public name: string, 
+//         public age: number, 
+//     ){
+//     }
+// }
+
+// const person: Person = new Person("John", 20)
+// console.log(person);
+
+
+function first(){
+    console.log("first(): factory evaluated");
+    return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+        console.log("first(): called");
+        console.log(target);
+        console.log(propertyKey);
+        console.log(descriptor);
+        console.log("---");
+        
     }
-
-    // @StaticMethodDecorator
-    public static seatCount(): void{
-        console.log("150 seats");
-    }
-
-    // @MethodDecorator
-    public pilotName(name: string, /*@ParameterDecorator lastName: string*/) {
-        console.log(name);
-    }
-
-    @AccessorDecorator
-    public get aircraftModel() {
-        return this._aircraftModel
+}
+function second(){
+    console.log("second(): factory evaluated");
+    return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+        console.log("second(): called");
+        console.log(target);
+        console.log(propertyKey);
+        console.log(descriptor);
+        console.log("---");
     }
 }
 
-
-
-//for static method or property, you wil always get constructor as first argument in decorators
-
-
-interface MapLocation {
-    lat: number,
-    long: number
+class ExampleClass{
+    @first()
+    @second()
+    method(){}
 }
-
-function AddLocation(lat: number, long: number){
-    return <T extends {new(...args: any[]): {}}>(classConstructor: T)=>{
-        return class extends classConstructor{
-            public mapLocation: MapLocation;
-            constructor(...args: any[]){
-                super(...args)
-                this.mapLocation = {lat, long}
-            }
-        }
-    }
-}
-
-@AddLocation(1.234, 1.876)
-class Person{
-    constructor(
-        public name: string, 
-        public age: number, 
-    ){
-    }
-}
-
-const person: Person = new Person("John", 20)
-console.log(person);
