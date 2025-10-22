6bad270462e55f9c30d0adafc2a1bac4e893311f
abhinavgautam01 <abgautam1017@gmail.com>
2025-10-22 21:30:38 +0530

    use-context

--- prac-react-4/src/App.jsx
+++ prac-react-4/src/App.jsx
@@ -1,18 +1,69 @@
 import { useState, createContext } from "react";
 import "./App.css";
 import { useContext } from "react";
-
+import { atom, RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+// context_api
 const BulbContext = createContext();
-function App() {
-  const [bulbOn, setBulbOn] = useState(false);
+
+//state_management_api
+const countState = atom({
+  key: 'countState',
+  default: 0,
+});
+
+function Parent() {
+  return (
+    <RecoilRoot>
+      <Increase />
+      <Decrease />
+      <Value />
+    </RecoilRoot>
+  );
+}
+function Increase() {
+  const setCount = useSetRecoilState(countState);
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>Increase</button>
+  );
+}
+function Decrease() {
+  const setCount = useSetRecoilState(countState);
+  return (
+    <button onClick={() => setCount((count) => count - 1)}>Decrease</button>
+  );
+}
+
+function Value() {
+  const count = useRecoilValue(countState);
   return (
     <div>
-    <BulbContext.Provider value={{
-      bulbOn: bulbOn,
-      setBulbOn: setBulbOn
-    }}>
-      <Light />
+      <p>Count: {count}</p>
+    </div>
+  );
+}
+function BulbProvider({ children }) {
+  const [bulbOn, setBulbOn] = useState(false);
+  return (
+    <BulbContext.Provider
+      value={{
+        bulbOn: bulbOn,
+        setBulbOn: setBulbOn,
+      }}
+    >
+      {children}
     </BulbContext.Provider>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <BulbProvider>
+        <Light />
+      </BulbProvider>
+      <div>
+        <Parent />
+      </div>
     </div>
   );
 }
@@ -27,9 +78,9 @@ function Light() {
 }
 
 function LightBulb() {
-  const { bulbOn } = useContext(BulbContext)
+  const { bulbOn } = useContext(BulbContext);
   return (
-    <div style={{marginBottom: 10}}>
+    <div style={{ marginBottom: 10 }}>
       {bulbOn ? (
         <img
           src="https://cdn-icons-png.flaticon.com/128/702/702797.png"
@@ -51,7 +102,7 @@ function LightBulb() {
 function LightSwitch() {
   const { setBulbOn } = useContext(BulbContext);
   function toggle() {
-    setBulbOn(currentState => !currentState);
+    setBulbOn((currentState) => !currentState);
   }
   return <button onClick={toggle}>Toggle </button>;
 }
