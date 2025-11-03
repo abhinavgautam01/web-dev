160de7243acaef6d321eec30c938541367aec931
abhinavgautam01 <abgautam1017@gmail.com>
2025-11-03 21:10:28 +0530

    pick-api-typescript

--- typescript-prac/src/index.ts
+++ typescript-prac/src/index.ts
@@ -1,24 +1,48 @@
-console.log("Types and interfaces")
+// console.log("Types and interfaces")
 
-interface GoodUser {
-    name: string,
-    age: number,
-    email: string
-}
+// interface GoodUser {
+//     name: string,
+//     age: number,
+//     email: string
+// }
 
-interface BadUser {
+// interface BadUser {
+//     name: string,
+//     age: number,
+//     email: string,
+//     ip: string
+// }
+
+// // intersection of two interfaces....this may look like union but it is intersection
+// type Users = GoodUser & BadUser
+
+// let user1: Users = {
+//     name: "golu",
+//     age: 21,
+//     email: "golu@gmail.com",
+//     ip: "0.1.2.123.1"
+// }
+
+interface User {
+    id: string,
     name: string,
     age: number,
     email: string,
-    ip: string
+    password: string
 }
 
-// intersection of two interfaces....this may look like union but it is intersection
-type Users = GoodUser & BadUser
-
-let user1: Users = {
-    name: "golu",
+let user: User = {
+    id: "1",
+    name: "Golu",
     age: 21,
     email: "golu@gmail.com",
-    ip: "0.1.2.123.1"
+    password: "asdf"
 }
+
+type UpdatedProps = Pick<User, "name" | "age" | "email" >
+
+function logDetails(user: UpdatedProps) {
+    console.log(`Name: ${user.name}, Age: ${user.age}, Email: ${user.email}`)
+}
+
+logDetails(user);
