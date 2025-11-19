8ee4a7f4d3c30bea17336ba351d1d2d3a38b6bab
abhinavgautam01 <abgautam1017@gmail.com>
2025-11-19 21:28:37 +0530

    ws

--- web-sockets-chat-application/server/src/index.ts
+++ web-sockets-chat-application/server/src/index.ts
@@ -1,7 +1,19 @@
-import { WebSocketServer } from "ws"
+import { WebSocketServer, WebSocket } from "ws"
 
 const wss = new WebSocketServer({port: 8080})
 
+let userCount = 0
+let allSockets: WebSocket[] = []
+
 wss.on("connection", (socket)=>{
-    console.log("user connected")
+    allSockets.push(socket)
+    userCount = userCount + 1;
+    console.log(`user${userCount} connected`)
+
+    socket.on("message", (message)=>{
+        console.log(`message recieved ${message.toString()}`)
+        allSockets.forEach(socket => {
+            socket.send(message.toString() + " from the server")
+        });
+    })
 })
