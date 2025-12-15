758aca3aff55b608220a1608d1b8977422425ca7
abhinavgautam01 <abgautam1017@gmail.com>
2025-12-15 21:51:19 +0530

    layouts

---
+++ nextjs-prac/src/app/posts/page.tsx
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+async function getTodos(){
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos")   
+    return response.data
+}
+
+interface ITodo {
+    title: string,
+    completed: boolean
+}
+
+function Todo({title, completed}: ITodo){
+    return (
+        <div>
+            {title} {completed ? "done" : "not done!"}
+        </div>
+    )
+}
+
+
+
+export default async function Todos(){
+    const todos = await getTodos()
+    return (
+        <div>
+            {/*@ts-ignore*/}
+            {todos.map(todo=><Todo key={todo.title} title={todo.title} completed={todo.completed} />)}
+        </div>
+    )
+}
