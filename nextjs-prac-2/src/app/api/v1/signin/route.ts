398006ec6a6907e4bb64cf5ad8443660a00f1c58
abhinavgautam01 <abgautam1017@gmail.com>
2026-01-03 20:31:45 +0530

    backend-route

--- nextjs-prac-2/src/app/api/v1/signin/route.ts
+++ nextjs-prac-2/src/app/api/v1/signin/route.ts
@@ -1,9 +1,17 @@
+import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest){
     const data = await req.json()
+    console.log(data)
 
-    
+    prisma.user.create({
+        data: {
+            username: data.username,
+            password: data.password
+        }
+    })
+
     return NextResponse.json({
         message: "Sign In Successfull"
     })
